79e5f7fce27dae5228cc1ed1835c4cc7b0111aa3
vkorablev <vovateleport@gmail.com>
2015-08-06 15:30:05 +0300

    import+

--- import/import.js
+++ import/import.js
@@ -9,8 +9,6 @@ var path = require('path');
 var args = process.argv.slice(2);
 var configPath = args.length>0?args[0]:'test.hjson';
 
-//var fileConfig=fs.readFileSync(path.resolve(__dirname,configPath),{encoding:'utf8'});
-//var c = hjson.parse(fileConfig);
 var c = require("./testc");
 
 var baseDir = c.baseDir||__dirname;
@@ -24,12 +22,9 @@ cmd.download = `wget -O ${t.name}_src.osm.pbf ${t.file}`;
 cmd.osmosis = `osmosis -v --read-pbf ./${t.name}_src.osm.pbf --bounding-box top=${t.bbox.top} left=${t.bbox.left} bottom=${t.bbox.bottom} right=${t.bbox.rigth} completeWays=yes --lp --write-pbf ${t.name}.osm.pbf`;
 cmd.to_sql = `osm2pgsql -d ${t.name} ${t.name}.osm.pbf -P 5432 -U robosm --cache-strategy sparse -C 500 --style ${styleFileFullPath}`;
 
-console.log('pwd:',sh.pwd());
 sh.cd(sh.pwd());
 sh.cd('..');
 var pwd1 = sh.pwd();
-console.log('pwd2:',sh.pwd());
-console.log('_tt', JSON.stringify([pwd1,baseDir,pwd1==baseDir,pwd1===baseDir]));
 sh.cd(baseDir);
 sh.mkdir('-p','build');
 sh.cd('build');
@@ -48,7 +43,7 @@ function execPromise(command){
 			console.log('Exit code:', code);
 			console.log('Program output:', output);
 			ok();
-		}).output.to('import.log');
+		});
 	});
 }
 
