c5fa52a6132a31f890789e5e006269f6300ec19f
vkorablev <vovateleport@gmail.com>
2015-08-06 15:42:35 +0300

    import+

--- import/import.js
+++ import/import.js
@@ -10,23 +10,18 @@ var args = process.argv.slice(2);
 var configPath = args.length>0?args[0]:'test.hjson';
 
 var c = require("./testc");
-
 var baseDir = c.baseDir||__dirname;
-
 var styleFileFullPath = path.resolve(baseDir, c.stylesFile);
 
 var t = c.tasks[0];
 
 var cmd = {};
-cmd.download = `wget -O ${t.name}_src.osm.pbf ${t.file}`;
-cmd.osmosis = `osmosis -v --read-pbf ./${t.name}_src.osm.pbf --bounding-box top=${t.bbox.top} left=${t.bbox.left} bottom=${t.bbox.bottom} right=${t.bbox.rigth} completeWays=yes --lp --write-pbf ${t.name}.osm.pbf`;
-cmd.to_sql = `osm2pgsql -d ${t.name} ${t.name}.osm.pbf -P 5432 -U robosm --cache-strategy sparse -C 500 --style ${styleFileFullPath}`;
+cmd.download = `wget -O ${t.name}_src.osm.pbf ${t.file} > output.log`;
+cmd.osmosis = `osmosis -v --read-pbf ./${t.name}_src.osm.pbf --bounding-box top=${t.bbox.top} left=${t.bbox.left} bottom=${t.bbox.bottom} right=${t.bbox.rigth} completeWays=yes --lp --write-pbf ${t.name}.osm.pbf >> output.log`;
+cmd.to_sql = `osm2pgsql -d ${t.name} ${t.name}.osm.pbf -P 5432 -U robosm --cache-strategy sparse -C 500 --style ${styleFileFullPath} >> output.log`;
 
-sh.cd(sh.pwd());
-sh.cd('..');
-var pwd1 = sh.pwd();
 sh.cd(baseDir);
-sh.mkdir('-p','build');
+sh.mkdir('-p',`build`);
 sh.cd('build');
 sh.mkdir('-p', t.name);
 sh.cd(t.name);
@@ -36,6 +31,7 @@ runGenerator(function*(){
 	yield execPromise(cmd.osmosis);
 	yield execPromise(cmd.to_sql);
 });
+
 
 function execPromise(command){
 	return new Promise(function(ok,fail){
@@ -49,7 +45,6 @@ function execPromise(command){
 
 function runGenerator(g) {
 	var it = g(), ret;
-
 	// asynchronously iterate over generator
 	(function iterate(val){
 		ret = it.next( val );
