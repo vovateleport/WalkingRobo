76a4ad264c89e1d38fc674487df4751ad282df9f
vkorablev <vovateleport@gmail.com>
2015-08-06 16:09:25 +0300

    import+

--- import/import.js
+++ import/import.js
@@ -5,35 +5,43 @@ sh.config.fatal = true;
 var hjson = require('hjson');
 var fs = require('fs');
 var path = require('path');
-
-var args = process.argv.slice(2);
-var configPath = args.length>0?args[0]:'test.hjson';
-
 var c = require("./testc");
-var baseDir = c.baseDir||__dirname;
-var styleFileFullPath = path.resolve(baseDir, c.stylesFile);
-
-var t = c.tasks[0];
 
 var cmd = {};
-cmd.download = `wget -O ${t.name}_src.osm.pbf ${t.file} > output.log`;
-cmd.osmosis = `osmosis -v --read-pbf ./${t.name}_src.osm.pbf --bounding-box top=${t.bbox.top} left=${t.bbox.left} bottom=${t.bbox.bottom} right=${t.bbox.rigth} completeWays=yes --lp --write-pbf ${t.name}.osm.pbf >> output.log`;
-cmd.to_sql = `osm2pgsql -d ${t.name} ${t.name}.osm.pbf -P 5432 -U robosm --cache-strategy sparse -C 500 --style ${styleFileFullPath} >> output.log`;
-
-sh.cd(baseDir);
-sh.mkdir('-p',`build`);
-sh.cd('build');
-sh.mkdir('-p', t.name);
-sh.cd(t.name);
 
 runGenerator(function*(){
+	yield new Promise(function(ok){
+		prepare();
+		ok();
+	});
 	yield execPromise(cmd.download);
 	yield execPromise(cmd.osmosis);
 	yield execPromise(cmd.to_sql);
+	yield new Promise(function(ok){
+		console.log('finish');
+		ok();
+	});
+
 });
 
-console.log('finish!');
+function prepare() {
+	var args = process.argv.slice(2);
+	var configPath = args.length > 0 ? args[0] : 'test.hjson';
 
+	var baseDir = c.baseDir || __dirname;
+	var styleFileFullPath = path.resolve(baseDir, c.stylesFile);
+
+	var t = c.tasks[0];
+	cmd.download = `wget -O ${t.name}_src.osm.pbf ${t.file}`;
+	cmd.osmosis = `osmosis -v --read-pbf ./${t.name}_src.osm.pbf --bounding-box top=${t.bbox.top} left=${t.bbox.left} bottom=${t.bbox.bottom} right=${t.bbox.rigth} completeWays=yes --lp --write-pbf ${t.name}.osm.pbf`;
+	cmd.to_sql = `osm2pgsql -d ${t.name} ${t.name}.osm.pbf -P 5432 -U robosm --cache-strategy sparse -C 500 --style ${styleFileFullPath}`;
+
+	sh.cd(baseDir);
+	sh.mkdir('-p', `build`);
+	sh.cd('build');
+	sh.mkdir('-p', t.name);
+	sh.cd(t.name);
+}
 
 function execPromise(command){
 	return new Promise(function(ok,fail){
@@ -55,7 +63,11 @@ function runGenerator(g) {
 			// poor man's "is it a promise?" test
 			if ("then" in ret.value) {
 				// wait on the promise
-				ret.value.then( iterate );
+				ret.value
+					.then(iterate)
+					.catch(function(err){
+						throw err;
+					});
 			}
 			// immediate value: just send right back in
 			else {
