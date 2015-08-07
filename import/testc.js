8e34038bfc6308a7fccac2f1d848f0cb0f07c3f0
vkorablev <vovateleport@gmail.com>
2015-08-07 13:17:26 +0300

    clear

--- import/testc.js
+++ import/testc.js
@@ -6,7 +6,6 @@ module.exports = {
 			name: "azore",
 			file: "http://download.geofabrik.de/europe/azores-latest.osm.pbf",
 			bbox: {
-				//-27.1455001831,38.7216797752,-27.0253372192,38.7907537883
 				top: 38.7907537883,
 				bottom: 38.7216797752,
 				left: -27.0253372192,
@@ -17,11 +16,20 @@ module.exports = {
 			name: "azore2",
 			file: "http://download.geofabrik.de/europe/azores-latest.osm.pbf",
 			bbox: {
-				//-27.1455001831,38.7216797752,-27.0253372192,38.7907537883
 				top: 38.7907537883,
 				bottom: 38.7216797752,
 				left: -27.0253372192,
 				rigth: -27.1455001831
+			}
+		},
+		{
+			name: "gis",
+			file: "http://download.geofabrik.de/europe/russia-european-part-latest.osm.pbf",
+			bbox: {
+				top: 55.96,
+				bottom: 55.53,
+				left: 37.34,
+				rigth: 37.89
 			}
 		}
 	]
--- web/app-entry.es6
+++
@@ -1,6 +0,0 @@
-require("bootstrap/dist/css/bootstrap.css");
-require("web/assets/app.css");
-import {App} from "web/app";
-
-new App().start();
-
